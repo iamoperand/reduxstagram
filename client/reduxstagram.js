44a79b555931e92f96a8a5f8f295d3dd977fa509
Nikhil Arora <narora200@gmail.com>
2017-08-08 22:24:43 +0530

    Basic Hello App

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,10 @@
 // let's go!
+
+import React from 'react';
+import { render } from 'react-dom';
+
+//import css
+
+import css from './styles/style.styl';
+
+render(<p>Hello</p>, document.getElementById('root'));
