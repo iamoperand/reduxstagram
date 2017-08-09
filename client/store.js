f64fe206918173770ef49639db88efc1fb36e18c
Nikhil Arora <narora200@gmail.com>
2017-08-09 19:59:16 +0530

    configured the store in store.js

--- client/store.js
+++ client/store.js
@@ -1,1 +1,16 @@
-//store.js
+import { createStore } from 'redux';
+
+//import the root reducer
+import rootReducer from './reducers/index';
+
+//import the data
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create an object for the default data
+const defaultState = {
+  posts,
+  comments
+};
+
+const store = createStore(rootReducer, defaultState);
