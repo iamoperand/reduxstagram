50e29756a22e54fd1859329985105614327c6cef
Nikhil Arora <narora200@gmail.com>
2017-08-10 21:01:03 +0530

    reducers/comments.js: comments function defined

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -7,7 +7,12 @@ function postComments(state = [], action) {
   return state;
 }
 function comments(state = [], action) {
-
+  if(typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
