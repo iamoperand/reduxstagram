5298982ea310c6ffd74e20c5628c7a3aebe26d92
Nikhil Arora <narora200@gmail.com>
2017-08-10 20:59:29 +0530

    reducers/comments.js: added postComments reducer for reducer composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,7 +1,11 @@
 // a reducer takes in 2 things:
-// 1. the action (info about what happened)
-// 2. a copy of current state
+// 1. a copy of current state
+// 2. the action (info about what happened)
 
+function postComments(state = [], action) {
+
+  return state;
+}
 function comments(state = [], action) {
 
   return state;
