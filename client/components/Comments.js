4f48280281fe695191e1a0bafe1847fafc8c4c5c
Nikhil Arora <narora200@gmail.com>
2017-08-10 20:10:19 +0530

    Comments.js: comment-form form added

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -18,6 +18,11 @@ class Comments extends React.Component {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
       </div>
     )
   }
