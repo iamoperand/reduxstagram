9bc0763938a9bac3e8b94bcdd071efc8bcbdde9e
Nikhil Arora <narora200@gmail.com>
2017-08-10 21:04:13 +0530

    reducers/comments.js: postComments function: ADD_COMMENT action.type defined

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -5,6 +5,10 @@
 function postComments(state = [], action) {
   switch(action.type) {
     case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
     case 'REMOVE_COMMENT':
       return state;
     default:
