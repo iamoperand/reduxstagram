440d7aac1970587b0b325d184ef29a81a5150480
Nikhil Arora <narora200@gmail.com>
2017-08-10 20:22:55 +0530

    Comments.js: defined the variables in onSubmit handler

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -18,13 +18,17 @@ class Comments extends React.Component {
   handleSubmit(e){
     e.preventDefault();
     console.log("Submitting the form");
+    const { postId } = this.props.match.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    console.log(postId, author, comment);
   }
 
   render(){
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
