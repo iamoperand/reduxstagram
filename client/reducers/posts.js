c7d52e8ab46ff04980c0e5a17d61f76157b4e8c9
Nikhil Arora <narora200@gmail.com>
2017-08-10 17:23:42 +0530

    reducers/posts.js: comment added

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,9 +3,18 @@
 // 2. a copy of current state
 
 function posts(state = [], action) {
-  console.log("The post will load");
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+
+    default:
+    return state;
+  }
 }
+{/*
+  reducers don't mutate the state, they behave like pure functions to state.
+  The difference between impure and pure functions and how it affects the flow is
+  explained in the links below:
+  impure-functions: https://codepen.io/iamoperand/pen/NvgxJa/
+  pure-function: https://codepen.io/iamoperand/pen/vJZLML/
+*/}
 
 export default posts;
