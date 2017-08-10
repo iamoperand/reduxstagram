b03f34b1bc593baf77345dd680da88b10a5ceaa1
Nikhil Arora <narora200@gmail.com>
2017-08-10 09:12:48 +0530

    App.js: Smart component layout created

--- client/components/App.js
+++ client/components/App.js
@@ -2,3 +2,9 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import * as actionCreators from '../actions/actionCreators';
+
+import Main from './Main';
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Main);
+
+export default App;
