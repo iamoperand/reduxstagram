bb89311b44aa0cbb193d143a6fc813abcf622827
Nikhil Arora <narora200@gmail.com>
2017-08-10 15:41:05 +0530

    added key in this.props.posts.map to remove the warning

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,7 @@ class PhotoGrid extends React.Component {
   render(){
     return (
       <div className="photo-grid">
-          {this.props.posts.map((post, index)=> (<Photo />))}
+          {this.props.posts.map((post, index)=> (<Photo key={index}/>))}
       </div>
 
     )
