6f5232cca9c638829d652fc163eea0d5224448fb
Nikhil Arora <narora200@gmail.com>
2017-08-10 15:18:26 +0530

    JSON.stringify testing in Single component

--- client/components/Single.js
+++ client/components/Single.js
@@ -6,7 +6,7 @@ class Single extends React.Component {
   render(){
     return (
       <div className="single-photo">
-        I am the single
+        {JSON.stringify(this.props, null, ' ')}
       </div>
     )
   }
